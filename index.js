c7b279fc003b7817af2be6881dce8968bf392874
Prem Rajput <104985726+premrajput4501@users.noreply.github.com>
2024-09-05 12:08:29 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ document.querySelector(".dropdown-menu").addEventListener("mouseover",function()
     audio.play();
 })
 
+document.querySelector(".card").addEventListener("mouseover",function(){
+    var audio = new Audio('./Sounds/click1.wav');
+    audio.play();
+})
+
 
 document.querySelector("#navbuttonclose").addEventListener("click",function(){
     var audio = new Audio('./Sounds/coolclick.wav');
@@ -29,7 +34,7 @@ document.querySelector(".carousel-control-prev-icon").addEventListener("click",f
     audio.play();
 })
 
-document.querySelector(".navlogo").addEventListener("click",function(){
+document.querySelector(".navlo  go").addEventListener("click",function(){
     var audio = new Audio('./Sounds/slash.mp3');
     audio.play();
 })
